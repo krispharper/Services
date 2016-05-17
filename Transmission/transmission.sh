9305dad0926039d766a4faed0a1c4b6f45308dd5
Kris Harper <kris.p.harper@gmail.com>
2016-05-16 20:39:17 -0400

    Added custom user and group for the transmission-daemon user

--- Transmission/transmission.sh
+++ Transmission/transmission.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o debian-transmission
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o debian-transmission
 
 exec su -l debian-transmission -s /bin/bash -c "exec transmission-daemon \
             --config-dir /transmission/info --encryption-preferred \
