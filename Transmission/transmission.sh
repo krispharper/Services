6442e63e4b8f975a7e2298b3573146d3b5502fdb
Kris Harper <kris.p.harper@gmail.com>
2016-05-30 15:14:10 -0400

    Changed to use separate incomplete directory

--- Transmission/transmission.sh
+++ Transmission/transmission.sh
@@ -5,4 +5,4 @@
 exec su -l debian-transmission -s /bin/bash -c "exec transmission-daemon \
             --config-dir /transmission/info --encryption-preferred \
             --dht --foreground --log-error -e /dev/stdout --no-portmap \
-            --download-dir /transmission/downloads --incomplete-dir /transmission/downloads"
+            --download-dir /transmission/downloads --incomplete-dir /transmission/incomplete"
